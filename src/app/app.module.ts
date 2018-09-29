bd7a69e1408e85168a76c0a21d7b639b362d420d
Muhammad Junaid <muhammadjunaidhassan1@gmail.com>
2018-09-30 00:13:21 +0500

    First commit, Initialization of app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './entry/login/login.component';
+import { RegisterationComponent } from './entry/registeration/registeration.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterationComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'login', pathMatch: 'full'},
+      {path: 'login', component: LoginComponent}
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAbZplqw7rUDD2Nha-hkNI8-EAHh0J771c",
+    authDomain: "handyman-ea0e1.firebaseapp.com",
+    databaseURL: "https://handyman-ea0e1.firebaseio.com",
+    projectId: "handyman-ea0e1",
+    storageBucket: "handyman-ea0e1.appspot.com",
+    messagingSenderId: "221396946660"
+  }
 };
 
 /*
